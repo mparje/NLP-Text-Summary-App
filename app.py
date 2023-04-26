f9e61db27b29b51867d9668eda6534e0f5f02eb6
mparje <126131806+mparje@users.noreply.github.com>
2023-04-26 03:39:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,28 +13,20 @@ from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-# import nltk
-# import ssl
-# try:
-#      _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
-# nltk.download()
+import PyPDF2
 
 
-# Functions for resume in Portuguese
-stopwords_ptbr = set(stopwords.words('portuguese') + list(punctuation))
+# Functions for resume in Spanish
+stopwords_es = set(stopwords.words('spanish') + list(punctuation))
 
 
-def remove_stopwords_and_punct_in_portuguese(text):
+def remove_stopwords_and_punct_in_spanish(text):
     words = word_tokenize(text.lower())
-    return [word for word in words if word not in stopwords_ptbr]
+    return [word for word in words if word not in stopwords_es]
 
 
-def sumarize_text_portuguese(text, n_sent=2):
-    words_not_stopwords = remove_stopwords_and_punct_in_portuguese(text)
+def summarize_text_spanish(text, n_sent=2):
+    words_not_stopwords = remove_stopwords_and_punct_in_spanish(text)
     sentences = sent_tokenize(text)
     frequency = FreqDist(words_not_stopwords)
     important_sentences = defaultdict(int)
@@ -53,74 +45,32 @@ def sumarize_text_portuguese(text, n_sent=2):
         st.write(sentences[i])
 
 
-# Functions for resume in English
-stopwords_en = set(stopwords.words('english') + list(punctuation))
-
-
-def remove_stopwords_and_punct_in_english(text):
-    words = word_tokenize(text.lower())
-    return [word for word in words if word not in stopwords_en]
-
-
-def sumarize_text_english(text, n_sent=2):
-    words_not_stopwords = remove_stopwords_and_punct_in_english(text)
-    sentences = sent_tokenize(text)
-    frequency = FreqDist(words_not_stopwords)
-    important_sentences = defaultdict(int)
-
-    for i, sentence in enumerate(sentences):
-        for word in word_tokenize(sentence.lower()):
-            if word in frequency:
-                important_sentences[i] += frequency[word]
-
-    numb_sent = n_sent
-    idx_important_sentences = nlargest(numb_sent,
-                                       important_sentences,
-                                       important_sentences.get)
-
-    for i in sorted(idx_important_sentences):
-        return st.write(sentences[i])
-
-
 def main():
-    st.title('Summarize texts with NLP :hugging_face:')
-    st.markdown("<h4 '>Enter your text and choose a language and a series of sentences that you consider important for your summary!\
-        The rest is done by natural language processing and statistics!</h4>", unsafe_allow_html=True)
+    st.title('Resumen de textos con NLP :hugging_face:')
+    st.markdown("<h4 '>¡Sube un archivo en formato PDF y elige el número de frases que consideras importantes para\
+        tu resumen! ¡El procesamiento de lenguaje natural y las estadísticas harán el resto!</h4>", unsafe_allow_html=True)
     st.write('')
 
-    uploaded_file = st.text_area('Type or paste here:',
-                                 'Processamento de linguagem natural (NLP, em inglês) é uma subárea da ciência da\
-                                  computação, inteligência artificial e da linguística que estuda os problemas da\
-                                  geração e compreensão automática de linguas humanas naturais. Sistemas de geração\
-                                  de linguagem natural convertem informação de bancos de dados de computadores em\
-                                  linguagem compreensível ao ser humano e sistemas de compreensão de linguagem natural\
-                                  convertem ocorrências de linguagem humana em representações mais formais, mais\
-                                  facilmente manipuláveis por programas de computador. Alguns desafios do NLP são\
-                                  compreensão de linguagem natural, fazer com que computadores extraiam sentido de\
-                                  linguagem humana ou natural e geração de linguagem natural.', height=300)
+    # Upload file
+    file_uploaded = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-    if uploaded_file is not None:
-        # To convert to a string based IO:
-        # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        # To read file as string:
-        # uploaded_file = stringio.read()
+    if file_uploaded is not None:
+        pdf_reader = PyPDF2.PdfFileReader(file_uploaded)
+        full_text = ""
+        for page in range(pdf_reader.getNumPages()):
+            full_text += pdf_reader.getPage(page).extractText()
 
         # Sidebar Menu
-        options = ["Portuguese", "English"]
-        menu = st.sidebar.selectbox("Choose a language:", options)
+        options = ["Español"]
+        menu = st.sidebar.selectbox("Elige el idioma:", options)
 
         # Choices
-        if menu == "Portuguese":
-            st.title("This is the summary of your text :unlock:")
-            n_sent = st.sidebar.slider('Choose a number of important sentences:', value=1)
-            sumarize_text_portuguese(uploaded_file, n_sent)
+        if menu == "Español":
+            st.title("Este es el resumen de tu texto :unlock:")
+            n_sent = st.sidebar.slider('Elige el número de frases importantes:', value=1)
+            summarize_text_spanish(full_text, n_sent)
 
-        if menu == "English":
-            st.header("English Language. \n \n This is the summary of your text.")
-            n_sent = st.sidebar.slider('Number of sentences (default is 2).', value=1)
-            sumarize_text_english(uploaded_file, n_sent)
-
-        st.sidebar.info('Check out the project on [Github](https://github.com/marinaramalhete/NLP-Text-Summary-App)')
+        st.sidebar.info('Revisa el proyecto en [Github](https://github.com/marinaramalhete/NLP-Text-Summary-App)')
 
 
 if __name__ == '__main__':
